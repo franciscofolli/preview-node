cdb22fe08568682bf7bfc38d446b02732a666392
Francisco Folli <franciscoabel2001@gmail.com>
2021-08-24 05:20:33 -0300

    raising the crud structure using ClientController as ref

--- src/app/controllers/api/contract/ContractType.js
+++ src/app/controllers/api/contract/ContractType.js
@@ -5,8 +5,80 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req,res) => {
-    res.send({ ok: true, user: req.userId })
+router.get('/', async (req,res) => {
+    try {
+        const clients = await ClientSchema.find();
+
+        res.send({ clients, user: req.userId })
+    } catch (err) {
+        return res.status(400).send({ error: 'Error - Could not load Clients!', errorLog: { err } })
+    }
+    
 })
+
+router.post('/', async (req,res) => {
+    const { name, email } = req.body;
+    const userAudit = req.userId
+    const now = new Date();
+    try {
+
+        const client = await ClientSchema.create({
+            name,
+            email,  
+            persistDate: now,
+            userAudit
+        });
+
+        client.userAudit = undefined;
+
+        return res.send({ client: client, user: req.userId })
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Error - Could not create new client!', errorLog: { err } })
+    }
+})
+
+router.put('/:typeId', async (req,res) => {
+    const clientId = req.params.clientId;
+    const now = new Date();
+    try {
+        if(!clientId) {
+            return res.status(400).send({ error: 'Error - Please pass the clientId on pathVariable!', errorLog: { err } })
+        }
+
+        const client = await ClientSchema.findById(clientId);
+
+        if(!client) {
+            return res.status(404).send({ error: 'Error - Client not found!', errorLog: { err } })
+        }
+
+        await client.updateOne({
+            ...req.body, userAudit : req.userId, persistDate: now
+        });
+
+        client.userAudit = undefined;
+
+        return res.send({ sucess: 'Sucess - Client info changed sucessfully', user: req.userId })
+    } catch (err) {
+        return res.status(400).send({ error: 'Client List failed!', errorLog: { err } })
+    }
+})
+
+router.delete('/:typeId', async (req,res) => {
+    const clientId = req.params.clientId;
+    try {
+        if (clientId) {
+            await ClientSchema.findByIdAndRemove(clientId);
+            res.send({ error: 'Success - Client removed successfully', user: req.userId })      
+        } else {
+            res.status(400).send({ error: 'Error - Get parameter is empty', user: req.userId })
+        }
+        
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Client remove failed!' })
+    }
+})
+
 
 module.exports = app => app.use('/contract/type', router);
