51d2681283500afe5855a9289a80171f84ce2d65
Francisco Folli <franciscoabel2001@gmail.com>
2021-08-24 09:38:02 -0300

    creating contracts and their installments

--- src/app/controllers/api/contract/ContractController.js
+++ src/app/controllers/api/contract/ContractController.js
@@ -1,12 +1,113 @@
 const express = require('express');
 const authMiddleware = require('../../../middlewares/auth');
+const ContractSchema = require('../../../persistence/schemas/contract/ContractSchema');
+const ContractInstallmentSchema = require('../../../persistence/schemas/contract/ContractInstallmentSchema');
 
 const router = express.Router();
 
+
+
+
+
+
 router.use(authMiddleware);
 
-router.get('/', (req,res) => {
-    res.send({ ok: true, user: req.userId })
+router.get('/', async (req,res) => {
+    try {
+        const contractStatus = await ContractSchema.find().populate('contractInstallments');
+
+        res.send({ contractStatus, user: req.userId })
+    } catch (err) {
+        return res.status(400).send({ error: 'Error - Could not load Contracts!', errorLog: { err } })
+    }
+    
 })
+
+router.post('/', async (req,res) => {
+    const { contract, installmentNumbers } = req.body;
+    const now = new Date();
+    try {
+        const contractSaved = await ContractSchema.create({
+            ...contract,
+            persistDate: now,
+            userAudit: req.userId
+        });
+
+        const installmentObject = {
+            contract: contractSaved._id,
+            principalValue: (contractSaved.principalValue / installmentNumbers).toFixed(2),
+            interestValue: 0,
+            interestDueValue: 0,
+            installmentTotalValue: (contractSaved.totalDebt / installmentNumbers).toFixed(2),
+            persistDate: now,
+            userAudit: req.userId
+        }
+
+        let installments = [];
+        let date = contractSaved.openingDate;
+
+        for(var x = 1 ; x <= installmentNumbers ; x++){
+            const installmentSchema = new ContractInstallmentSchema({ ...installmentObject, installmentNumber: x,
+                dueDate: date.setMonth(contractSaved.openingDate.getMonth()+1)});
+            installments.push(installmentSchema);
+        }
+        
+        await Promise.all(installments.map(async installment => {
+            await installment.save();
+            contractSaved.contractInstallments.push(installment);
+        }));
+
+        await contractSaved.save();
+        
+        contractSaved.userAudit = undefined;
+
+        return res.send({ contract: contractSaved.populate('contractInstallments'), user: req.userId })
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Error - Could not create new Contract!'})
+    }
+})
+
+router.put('/:contractId', async (req,res) => {
+    const contractId = req.params.contractId;
+    const now = new Date();
+    try {
+        if(!contractId) {
+            return res.status(400).send({error: 'Error - Please pass the typeId on pathVariable!'})
+        }
+
+        const contract = await ContractSchema.findById(contractId);
+
+        if(!contract) {
+            return res.status(404).send({ error: 'Error - Contract not found!'})
+        }
+
+        await contractStatus.updateOne({
+            ...req.body, userAudit : req.userId, persistDate: now
+        });
+
+        return res.send({ sucess: 'Sucess - Contract info changed sucessfully', user: req.userId })
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Contract change failed!'});
+    }
+})
+
+router.delete('/:contractId', async (req,res) => {
+    const contractId = req.params.contractId;
+    try {
+        if (contractId) {
+            await ContractSchema.findByIdAndRemove(contractId);
+            res.send({ error: 'Success - Contract removed successfully', user: req.userId })      
+        } else {
+            res.status(400).send({ error: 'Error - Get parameter is empty', user: req.userId })
+        }
+        
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Contract Status remove failed!' })
+    }
+})
+
 
 module.exports = app => app.use('/contract', router);
--- src/app/controllers/api/contract/ContractInstallmentController.js
+++ src/app/controllers/api/contract/ContractInstallmentController.js
@@ -1,12 +1,83 @@
 const express = require('express');
 const authMiddleware = require('../../../middlewares/auth');
+const ContractInstallmentSchema = require('../../../persistence/schemas/contract/ContractInstallmentSchema');
+
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req,res) => {
-    res.send({ ok: true, user: req.userId })
+router.get('/:contractId', async (req,res) => {
+    const contractId = req.params.contractId;
+    try {
+        const contractInstallments = await ContractInstallmentSchema.find();
+
+        res.send({ contractInstallments: contractInstallments, user: req.userId })
+    } catch (err) {
+        return res.status(400).send({ error: 'Error - Could not load Contract Installments!', errorLog: { err } })
+    }
+    
 })
+
+// router.post('/', async (req,res) => {
+//     const now = new Date();
+//     try {
+
+//         const status = await ContractStatusSchema.create({
+//             ...req.body,
+//             persistDate: now,
+//             userAudit: req.userId
+//         });
+
+//         status.userAudit = undefined;
+
+//         return res.send({ contractStatus: status, user: req.userId })
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(400).send({ error: 'Error - Could not create new Contract Status!'})
+//     }
+// })
+
+router.put('/:installmentId', async (req,res) => {
+    const installmentId = req.params.installmentId;
+    const now = new Date();
+    try {
+        if(!installmentId) {
+            return res.status(400).send({error: 'Error - Please pass the typeId on pathVariable!'})
+        }
+
+        const contractInstallment = await ContractInstallmentSchema.findById(installmentId);
+
+        if(!contractInstallment) {
+            return res.status(404).send({ error: 'Error - Contract Installment not found!'})
+        }
+
+        await contractInstallment.updateOne({
+            ...req.body, userAudit : req.userId, persistDate: now
+        });
+
+        return res.send({ sucess: 'Sucess - Contract Installment info changed sucessfully', user: req.userId })
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Contract Installment change failed!'});
+    }
+})
+
+router.delete('/:installmentId', async (req,res) => {
+    const installmentId = req.params.installmentId;
+    try {
+        if (installmentId) {
+            await ContractStatusSchema.findByIdAndRemove(installmentId);
+            res.send({ error: 'Success - Contract Installment removed successfully', user: req.userId })      
+        } else {
+            res.status(400).send({ error: 'Error - Get parameter is empty', user: req.userId })
+        }
+        
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send({ error: 'Contract Installment remove failed!' })
+    }
+})
+
 
 module.exports = app => app.use('/contract/installment', router);
